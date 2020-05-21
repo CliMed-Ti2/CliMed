c309da4c4faefdd372713a216924194069d55608
Ruben Martins <rubenmachadomartins48@gmail.com>
2020-05-21 23:16:28 +0100

    Refeito Modelo: Clinicas

--- CliMed/CliMed/Models/Clinicas.cs
+++ CliMed/CliMed/Models/Clinicas.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace CliMed.Models
 {
     /// <summary>
@@ -14,48 +13,71 @@ namespace CliMed.Models
         //inicializar lista de materiais
         public Clinicas()
         {
-            ListaDeMateriais = new HashSet<Materiais>();
+
         }
-
-
         /// <summary>
         /// Id da Clinica
         /// </summary>
-        [Key] 
+        [Key]
         public int IdClinica { get; set; }
-
         /// <summary>
-        /// Morada da Clinica
+        /// Identificação da Rua onde a Clinica Existe
+        /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
+        [StringLength(60)]
+        [RegularExpression("[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]", ErrorMessage = "Só são aceites nomes, começados por letra Maiúscula, separados entre si por um espaço em branco.")]
+        public String Rua { get; set; }
+        /// <summary>
+        /// Identificação do Numero da Porta da Clinica
+        /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
+        [Display(Name = "Numero da Porta")]
+        public int nPorta { get; set; }
+        /// <summary>
+        /// Numero de Andar da Clinica (Opcional ou não ) -> Restrição Exemplo "5D"
+        /// </summary>
+        [Display(Name = "Numero da Andar")]
+        [StringLength(3)]
+        [RegularExpression("[+][1-9]{1,2}[A-Z]{1}", ErrorMessage = "Exemplo de formato de preenchimento: 3D")]
+        public String nAndar { get; set; }
+        /// <summary>
+        /// Codigo Postal da Clinica
+        /// </summary>
+        [Required]
+        [RegularExpression("[1-9][0-9]{3}-[0-9]{3}", ErrorMessage = "Deve inicialmente introduzir quatro numeros,sendo o primeiro de 1-9 e os restantes de 0-9, de seguida separar por tracinho(-) e por ultimo prencher três números de 0-9")]
+        [StringLength(8)]
+        [Display(Name = "Codigo Postal")]
+        public String CodPostal { get; set; }
+        /// <summary>
+        /// Localidade onde a clinica reside
         /// </summary>
         [Required(ErrorMessage = "A {0} é de preenchimento obrigatório!")]
-        [StringLength(40)]
-        public string Morada { get; set; }
-
+        [StringLength(20)]
+        [RegularExpression("[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]", ErrorMessage = "Só são aceites nomes, começados por letra Maiúscula, separados entre si por um espaço em branco.")]
+        public String Localidade { get; set; }
+        /// <summary>
+        /// Numero de Indentificação Fiscal da Clinca
+        /// </summary>
+        ///
+        [Display(Name = "Número dsse Identificação Fiscal")]
+        [StringLength(9)]
+        [RegularExpression("[1-9][0-9]{8}", ErrorMessage = "Deverá introduzir o NIF corretamente, contendo 9 numeros")]
+        public string NIF { get; set; }
         /// <summary>
         /// Contacto da Clinica
         /// </summary>
-        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
         [StringLength(9)]
         [RegularExpression("9[0-9]{8}", ErrorMessage = "O {0} deve ter 9 dígitos e começar por 9 ")]
-        public string Contacto { get; set; }
-
+        public String Contacto { get; set; }
         /// <summary>
-        /// Email da Clinica
+        /// Email correspondente a Clinica
         /// </summary>
-        [Required(ErrorMessage ="O {0} é de preenchimento obrigatório!")]
-        [StringLength(30, ErrorMessage ="O {0} deve ter no máximo {1} caracteres.")]
-        public string Mail { get; set; }
-
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
+        public string EMail { get; set; }
         /// <summary>
-        /// Fotografia do Estabelecimento Clinica 
+        /// Foto da Clinica
         /// </summary>
-        [StringLength(15,ErrorMessage ="A {0} não deverá ter mais de {1} caracteres.")]
-        public string Foto { get; set; }
-
-        /// <summary>
-        /// Lista de Materias de que a Clinica dispõe
-        /// </summary>
-        public ICollection<Materiais> ListaDeMateriais { get; set; }
-
+        public String Foto { get; set; }
     }
 }
